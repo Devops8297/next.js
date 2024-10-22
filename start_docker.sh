4f3ad573965f67086d195164b3053cc21af02748
Devops8297 <144135333+Devops8297@users.noreply.github.com>
2024-10-22 11:01:22 +0530

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# Stop any running containers
 echo "Stopping any running containers..."
 docker stop $(docker ps -q) || true
 
+# Remove old containers
 echo "Removing old containers..."
 docker rm $(docker ps -a -q) || true
 
+# Run new container
 echo "Running new container..."
 docker run -d -p 80:3000 --name nextjs-app "$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USERNAME/nextjs-app:latest"
